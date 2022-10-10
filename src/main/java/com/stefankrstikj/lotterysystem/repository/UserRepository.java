59dd6a408e2f2259ac00fbe88d1bafcd0ec047cf
Stefan Krstikj <st.krstic@gmail.com>
2022-10-11 00:28:54 +0200

    add @Repository annotation to UserRepository

--- src/main/java/com/stefankrstikj/lotterysystem/repository/UserRepository.java
+++ src/main/java/com/stefankrstikj/lotterysystem/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.stefankrstikj.lotterysystem.repository;
 
 import com.stefankrstikj.lotterysystem.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 }
