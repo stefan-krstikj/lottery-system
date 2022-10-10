79e8202852104e74c71107f6314aaba862a3a0f2
Stefan Krstikj <st.krstic@gmail.com>
2022-10-10 18:32:53 +0200

    initial commit

---
+++ src/main/java/com/stefankrstikj/lotterysystem/LotterySystemApplication.java
@@ -0,0 +1,13 @@
+package com.stefankrstikj.lotterysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotterySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotterySystemApplication.class, args);
+    }
+
+}
