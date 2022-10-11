5d4a7247e883e855550f2f5a1b4a1ba70307ecaa
Stefan Krstikj <st.krstic@gmail.com>
2022-10-11 19:41:33 +0200

    fix incorrect user ballot mapping in User

--- src/main/java/com/stefankrstikj/lotterysystem/model/User.java
+++ src/main/java/com/stefankrstikj/lotterysystem/model/User.java
@@ -29,7 +29,7 @@ public class User implements UserDetails {
     private String firstName;
     private String lastName;
 
-    @OneToMany
+    @OneToMany(mappedBy = "participant")
     private Set<LotteryBallot> ballots;
 
     private boolean isAccountNonExpired = true;
