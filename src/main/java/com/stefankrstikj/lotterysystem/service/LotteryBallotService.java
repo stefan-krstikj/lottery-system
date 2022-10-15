6daf1ebf381074071cdcef39941e5ae1a11eb0b9
Stefan Krstikj <st.krstic@gmail.com>
2022-10-15 14:47:29 +0200

    add usage comment for LotteryBallotService and LotteryService

--- src/main/java/com/stefankrstikj/lotterysystem/service/LotteryBallotService.java
+++ src/main/java/com/stefankrstikj/lotterysystem/service/LotteryBallotService.java
@@ -6,6 +6,10 @@ import com.stefankrstikj.lotterysystem.model.User;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * Service class for  {@link LotteryBallot}. This service should not be accessed directly, and instead please use
+ * {@link LotteryManagingService} to access the underlying logic here
+ */
 public interface LotteryBallotService {
     LotteryBallot create(LotteryBallot lotteryBallot);
 
--- src/main/java/com/stefankrstikj/lotterysystem/service/LotteryService.java
+++ src/main/java/com/stefankrstikj/lotterysystem/service/LotteryService.java
@@ -1,9 +1,14 @@
 package com.stefankrstikj.lotterysystem.service;
 
 import com.stefankrstikj.lotterysystem.model.Lottery;
+import com.stefankrstikj.lotterysystem.model.LotteryBallot;
 
 import java.time.LocalDate;
 
+/**
+ * Service class for  {@link Lottery}. This service should not be accessed directly, and instead please use
+ * {@link LotteryManagingService} to access the underlying logic here
+ */
 public interface LotteryService {
     Lottery save(Lottery lottery);
 
